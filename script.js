add466c3879522b454511ac6d54a451967375aec
CapnCoin <reynoldsmichael95@gmail.com>
2025-09-18 20:47:37 +0200

    REFACTORED refresh_content function
    -created new add_content function
        adds all content within MyLibrary to contents section
    
    -refresh_content function now only calls clear_content and add_content

--- script.js
+++ script.js
@@ -7,18 +7,38 @@ function clear_contents(){
     }
 }
 
-function refresh_contents(){
-    clear_contents();
+function add_contents(){
     for(let i = 0; i < MyLibrary.length; i++){
         //create card
         let new_book = document.createElement('div');
         //assign id and class
         new_book.id = `${MyLibrary[i].id}`;
         new_book.classList.add('book_card');
-        //add inner elements
+        //add title element
+        let title = document.createElement('div');
+        title.textContent = `Title: ${MyLibrary[i].title}`;
+        new_book.appendChild(title);
+        //add author element
+        let author = document.createElement('div');
+        author.textContent = `Author: ${MyLibrary[i].author}`;
+        new_book.appendChild(author);
+        //add page count
+        let n_pages = document.createElement('div');
+        author.textContent = `number of pages: ${MyLibrary[i].n_pages}`;
+        new_book.appendChild(n_pages);
+        //add have read
+        let have_read = document.createElement('div');
+        if(MyLibrary[i].have_read) have_read.textContent = 'You have read this book';
+        else have_read.textContent = 'You have not read this book';
+        new_book.appendChild(have_read);
 
         Contents.appendChild(new_book);
     }
+}
+
+function refresh_contents(){
+    clear_contents();
+    add_contents();
 }
 
 function Book(title, author, n_pages, have_read){
@@ -42,5 +62,5 @@ function add_book(title, author, n_pages, have_read){
 }
 
 add_book('test', 'test', 20, true);
-add_book('test', 'test', 20, true);
+add_book('test', 'test', 20, false);
 add_book('test', 'test', 20, true);
